6e4d7c3ab933aac60eda266a7374eb91a8029afb
aryan7081 <haryan458@gmail.com>
2025-03-18 11:57:12 +0530

    Updated procedure step

--- communityEmpowerment/admin.py
+++ communityEmpowerment/admin.py
@@ -145,12 +145,19 @@ class SchemeResource(resources.ModelResource):
         export_order = ('id', 'title', 'department__department_name', 'introduced_on', 'valid_upto', 'funding_pattern', 'description', 'scheme_link')
 
 
+
+class ProcedureInline(admin.TabularInline):
+    model = Procedure
+    extra = 1
+
+
 class SchemeAdmin(ImportExportModelAdmin):
     resource_class = SchemeResource
     list_display = ('title', 'department','is_active', 'introduced_on', 'valid_upto', 'funding_pattern')
     list_editable = ('is_active',) 
     search_fields = ('title', 'description','is_active')
     list_filter = ('department', 'introduced_on', 'valid_upto', 'funding_pattern','is_active')
+    inlines = [ProcedureInline]
 
     def is_active_toggle(self, obj):
         """ Show 'Active' / 'Inactive' with color styling """
