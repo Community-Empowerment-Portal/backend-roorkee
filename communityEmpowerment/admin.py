8828ff2965067082afab48ad1e96f12723d5a299
aryan7081 <haryan458@gmail.com>
2025-03-18 11:35:11 +0530

    Added admin control to profile field

--- communityEmpowerment/admin.py
+++ communityEmpowerment/admin.py
@@ -193,15 +193,15 @@ class ProfileFieldAdmin(admin.ModelAdmin):
     list_display = ('name', 'field_type', 'is_active','position',)
     list_filter = ['is_active', 'field_type']
     list_editable = ['is_active', 'position']
-    readonly_fields = ['name', 'field_type', 'placeholder', 'min_value', 'max_value']
+    # readonly_fields = ['name', 'field_type', 'placeholder', 'min_value', 'max_value']
     inlines = [ProfileFieldChoiceInline]
     def has_add_permission(self, request):
         """Disallow adding new fields."""
-        return False
+        return True
 
     def has_delete_permission(self, request, obj=None):
         """Disallow deleting fields."""
-        return False
+        return True
 admin_site.register(ProfileField, ProfileFieldAdmin)
 
 class ProfileFieldValueAdmin(admin.ModelAdmin):
